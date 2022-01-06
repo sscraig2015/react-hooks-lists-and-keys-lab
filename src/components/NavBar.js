76bb0d2428b2e25a4a1604bfef935532c8e26d8d
Sean <craig.sean92@gmail.com>
2022-01-05 18:56:41 -0700

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((section) => {
+    return <a key={section} href={`#${section}`}>{section}</a>
+  })
+
+  return (
+    <nav>{linkElements}</nav>
+  )
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,18 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projects.map((project) => {
+        
+        return <ProjectItem  name={project.name} about={project.about} technologies={project.technologies} key={project.id}/>
+        })}
+      </div>
     </div>
   );
 }
+
 
 export default ProjectList;
